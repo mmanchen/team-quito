0a4d3d75bd19905c0ad788c95095d48d74d8d52d
mmanchen <55111164+mmanchen@users.noreply.github.com>
2019-09-11 16:05:23 -0400

    Add files via upload

---
+++ maria-index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Maria Mancheno | Lab 1: Initial Personal Website </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Maria Mancheno </h1>
+			<img src= "images/photo-maria.jpg" alt="maria">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>University of Rochester</li>
+			<li>Senior</li>
+			<li>Digital Media Studies</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>My name is Maria Mancheno and I’m from Quito, Ecuador. I’m 21 years old and I currently go to the University of Rochester. My major is in Digital Media Studies and I’m also doing a minor in Math. My hobbies include playing soccer, playing the guitar and reading. I work as a social media manager and web developer at the Peak Lab in the University of Rochester Medical Center. This last summer I had the opportunity to do an internship in an Ecuadorian firm where I was able to improve my programming skills. My favorite thing is to travel around the world to learn about new cultures and meet new interesting people. 
+ </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
