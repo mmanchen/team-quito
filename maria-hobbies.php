c54d3775f1e13c4d4ebd44ec2d73b384de758d6c
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 15:56:59 -0400

    Create maria-hobbies.php

---
+++ maria-hobbies.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Maria Mancheno | Lab 1: Initial Personal Website </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.html">
+			<h1> Maria Mancheno </h1>
+			<img src= "images/photo-maria.jpg" alt="maria">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>University of Rochester</li>
+			<li>Senior</li>
+			<li>Digital Media Studies</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> University </h1>
+
+		<p>I began playing the guitar when I was 9 years old. I received private lessons at my house and the beginning it was a bit frustrating because it takes time until chords actually start to sound properly. Yet, I knew that instrument was meant for me and I never gave up. I play the guitar every single day and there is not a single place that I go without my guitar. It’s not only an instrument but one of the best possible companions. I began playing soccer even earlier, when I was about 7 years old. I think much of my soccer passion came from my dad’s influence, as he is a soccer lover. I play as a forward and I’m currently part of the club soccer team at the University of Rochester. Finally, my last hobby is reading. I’ve always liked to read, but I used to make up a lot of excuses and didn’t end up reading as much as I would have wanted. Therefore, my 2019 new year’s resolution included reading 24 book throughout the year. I made time to read at least 30 minutes every day. I finished my goal about three weeks ago and I’m currently on my 26th book now.  </p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
