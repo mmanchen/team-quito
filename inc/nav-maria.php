952742bd9ac2a7807c54693acaedb00fb8d00478
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 19:54:32 -0400

    Create nav-maria.php

---
+++ inc/nav-maria.php
@@ -0,0 +1,16 @@
+<nav>
+	<ul>
+		<li>
+			<a href="maria-index.php"> Me </a>
+		</li>
+		<li>
+			<a href="maria-high-school.php">High School</a>
+		</li>
+		<li>
+			<a href="maria-hobbies.php">Hobbies</a>
+		</li>
+		<li>
+			<a href="maria-university.php"> University </a>
+		</li>
+	</ul>	
+	</nav>
