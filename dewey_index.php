28f3a1a46bed3de535100d72eb57160aa9d42a9c
Baz D <deweybazirake@gmail.com>
2019-09-11 16:04:23 -0400

    Create dewey_index.php

---
+++ dewey_index.php
@@ -0,0 +1,52 @@
+
+<?php include "inc/html-top.inc"; ?>
+
+<div class="container">
+<body>
+
+
+
+<aside>
+	<a href="index.php"><img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake"></a>
+</aside>
+
+<section>
+
+<h1>Dewey Bazirake</h1>
+<h2>Data Scientist & Systems Analyst</h2>
+<h3>Python, Web Design & Machine Learning</h3>
+<h3>Business Analytics + Comp Sci </h3>
+
+</section>
+
+<header></header>
+
+
+
+<main>
+<p>
+
+I am an international student from Uganda, East Africa currently studying in the United States. My belief in education to make a positive change has always been a driving factor in my academics, work and travel. My hope is that to learn the necessary skills in computer science, finance and management to lead organizations and companies that will make a fundamental change in our world. We are becoming a more interconnected world thanks to technology, common language and easier transportation mechanisms. This gives me optimism as we can always work holistically as humans to advance in technology, protect our environment for future generations and lead a new era of sustainable prosperity. For more details into my background, technical skills, origin and my entrepreneurship journey, check out the other pages.
+</p>
+</main>
+
+
+
+<?php include "inc/nav.inc"; ?>
+<?php include "inc/scripts.php"; ?>
+
+
+
+
+<footer>
+
+	<p>CSC 174: Advanced Front-end Web</p>
+	<p>Design and Development</p>
+
+</footer>
+
+</div>
+</body>
+
+
+</html>
