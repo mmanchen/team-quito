6cd8009b4a58b80d02de4cbdf560ce6b71d697e8
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 15:57:02 -0400

    Create maria-university.php

---
+++ maria-university.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Maria Mancheno | Lab 1: Initial Personal Website </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.html">
+			<h1> Maria Mancheno </h1>
+			<img src= "images/photo-maria.jpg" alt="maria">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>University of Rochester</li>
+			<li>Senior</li>
+			<li>Digital Media Studies</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> University </h1>
+
+		<p>I came to the University of Rochester on August of 2016. My family came all the way from Ecuador to drop me in school. Those days were filled with mixed emotions, as I was excited to start a new stage of my life, but sad because I had to say goodbye to my family. When I first arrived, I wanted to study Economics, but from the beginning I wasn’t sure about it. I was going to give a try, but it ended up in disappointment. For this reason, I switched to Computer Science, it was my first time ever programming and it was really hard for me. I realized it wasn’t meant for me. Therefore, I decided to switch again to Mathematics, which is something I’ve always felt comfortable with. I did enjoy it a lot, but I wanted to combine with something else so that it would be more useful in the future. When exploring my career options, the most appealing option was Digital Media Studies. It combines many of my passions, such as website design, photography and film. Thus, I decided to major in Digital Media Studies and do a minor in Mathematics. </p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
