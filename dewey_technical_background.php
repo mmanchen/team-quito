430c712074f50c47d6c8fc12525efb10e6382b40
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 19:50:40 -0400

    Create dewey_technical_background.php

---
+++ dewey_technical_background.php
@@ -0,0 +1,70 @@
+
+
+<?php include "inc/html-top.inc"; ?>
+
+
+
+
+
+<body class="container">
+
+
+
+<header>
+	<a href="index.php/">
+		<h1>Dewey Bazirake</h1>
+		<img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake">
+	</a>
+</header>
+
+<?php include "inc/nav.inc"; ?>
+
+<aside>
+
+<ul>
+
+	<li>Dewey Bazirake</li>
+
+	<li>Data Scientist & Systems Analyst</li>
+
+	<li>Business Analytics + Comp Sci</li>
+
+</ul>
+
+</aside>
+
+
+
+
+<main>
+
+<h1>Innovative Works</h1>
+
+
+<p>
+	My love for numbers and making a change in the world has resulted in me taking an interest in Business, Computer Science & Robotics. During my Junior year, I went to study abroad in Shanghai, China which was a life changing experience. Aside from the great food that I enjoyed, I got a chance to put my python programming skills into practice. I worked with a startup called, MIRO AI - a sports AI company. They focused on image processing of athletes in marathons. It is through this experience I got a chance to learn more about Deep Learning, Artificial Intelligence & the power of Big Data. From then on, my life has never been the same again. I am always looking for opportunities to enhance my knowledge and skills in the Data & the Technology sector. The language I am getting fluent at: python, statistics, machine learning and databases. My hope is that I can work work with numerous companies who have access to large data so that I can do what I do best ~ organize it, analyze it & interpret it to derive the best value.
+
+</p>
+
+</main>
+
+
+
+
+
+<?php include "inc/nav.inc"; ?>
+<?php include "inc/scripts.php"; ?>
+
+
+<footer>
+
+	<p><strong>CSC 174: Advanced Front-end Web</strong></footer>
+
+
+</footer>
+
+
+</body>
+
+
+</html>
