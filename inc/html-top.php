612c15d1bed85779279689d16ad80eeaa42c5f5b
Xueying Chen <41633812+xchen74@users.noreply.github.com>
2019-09-11 16:01:00 -0400

    Add files via upload

---
+++ inc/html-top.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		
+		<title>Xueying Chen| Lab 1:Initial Personal Website </title>
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/menu-highlighter.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
---
+++ inc/nav.php
@@ -0,0 +1,16 @@
+<nav>
+	<ul>
+		<li>
+			<a href="index.php"> Home Page </a>
+		</li>
+		<li>
+			<a href="social-entrepreneurship.php">Social Entrepreneurship</a>
+		</li>
+		<li>
+			<a href="mindfulness-meditation.php"> Yoga and Meditation</a>
+		</li>
+		<li>
+			<a href="island-travel.php"> Becoming an island hopper </a>
+		</li>
+	</ul>	
+	</nav>
