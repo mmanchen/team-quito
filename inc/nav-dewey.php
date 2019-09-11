eba3fd53b43a8f8278d8d9268aeac8842b8b6def
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 19:50:42 -0400

    Create nav-dewey.php

---
+++ inc/nav-dewey.php
@@ -0,0 +1,16 @@
+<nav>
+	<ul>
+		<li>
+			<a href="dewey_index.php"> Me </a>
+		</li>
+		<li>
+			<a href="dewey_innovative_works.php">Innovative Works</a>
+		</li>
+		<li>
+			<a href="dewey_origin.php">Origin</a>
+		</li>
+		<li>
+			<a href="dewey_technical_background"> Technical Background </a>
+		</li>
+	</ul>	
+	</nav>
