137616f33a9300438bcab2f0db32fa6e6ad905eb
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 19:03:24 -0400

    Update dewey_index.php

--- dewey_index.php
+++ dewey_index.php
@@ -1,60 +1,42 @@
+</html>
 
-<?php include "inc/html-top.inc"; ?>
-
-<div class="container">
-<body>
-
-
-
-<aside>
-	<a href="index.php"><img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake"></a>
-</aside>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Dewey Bazirake </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
 
-    <aside>
-    	<ul>
-		<li>
-		Data Scientist & Systems Analyst
-		</li>
-		<li>
-		Python, Web Design & Machine Learning
-		</li>
-		<li>
-		Business Analytics + Comp Sci 
-		</li>
-		
-	</ul>	
-    </aside>	
+	<header>
+		<a href= "index.php">
+			<h1> MDewey Bazirake </h1>
+			<img src= "dewey_pic" alt="maria">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Data Scientist & Systems Analyst</li>
+			<li>Python, Web Design & Machine Learning</li>
+			<li>Business Analytics + Comp Sci </li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>I am an international student from Uganda, East Africa currently studying in the United States. My belief in education to make a positive change has always been a driving factor in my academics, work and travel. My hope is that to learn the necessary skills in computer science, finance and management to lead organizations and companies that will make a fundamental change in our world. We are becoming a more interconnected world thanks to technology, common language and easier transportation mechanisms. This gives me optimism as we can always work holistically as humans to advance in technology, protect our environment for future generations and lead a new era of sustainable prosperity. For more details into my background, technical skills, origin and my entrepreneurship journey, check out the other pages. 
+ </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
 	
 
-
-
-<main>
-	<h1>Dewey Bazirake</h1>
-<p>
-
-I am an international student from Uganda, East Africa currently studying in the United States. My belief in education to make a positive change has always been a driving factor in my academics, work and travel. My hope is that to learn the necessary skills in computer science, finance and management to lead organizations and companies that will make a fundamental change in our world. We are becoming a more interconnected world thanks to technology, common language and easier transportation mechanisms. This gives me optimism as we can always work holistically as humans to advance in technology, protect our environment for future generations and lead a new era of sustainable prosperity. For more details into my background, technical skills, origin and my entrepreneurship journey, check out the other pages.
-</p>
-</main>
-
-
-
-<?php include "inc/nav.inc"; ?>
-<?php include "inc/scripts.php"; ?>
-
-
-
-
-<footer>
-
-	<p>CSC 174: Advanced Front-end Web</p>
-	<p>Design and Development</p>
-
-</footer>
-
-</div>
-</body>
-
-
-</html>
+</body></html>
