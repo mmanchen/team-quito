e4c4281621c132aa62e77c5df896f8915b9ab2e3
Xueying Chen <41633812+xchen74@users.noreply.github.com>
2019-09-11 15:58:38 -0400

    Add files via upload

---
+++ shelley-social-entrepreneurship.php
@@ -0,0 +1,48 @@
+<?php include "inc/html-top.php"; ?>
+
+
+<body>
+	<div class="container"> 
+
+	<header>
+		<a href="index.php">	
+	    <h1> Xueying Chen </h1>
+	    <img src="images/shelley.jpg" alt="shelley-portrait">
+	    </a>
+    </header>
+
+    <aside>
+    	<ul>
+		<li>
+			Social Entrepreneur
+		</li>
+		<li>
+			Yoga and Meditation Enthusiast
+		</li>
+		<li>
+			Island Traveler
+		</li>
+		
+	</ul>	
+    </aside>
+   	
+
+ <?php include "inc/nav.php"; ?>
+ 
+    <main>
+    	<h1> Social Entrepreneurship </h1>
+    	 <p>I am a researcher, activist and writer of social entrepreneurship and social innovation. I worked on a research project in Beijing and Shenzhen investigating how Chinese social entrepreneurs help migrant workers integrate into urban society in China. The phenomenon of large-scale rural-urban migration has brought 250 million migrant workers into urban cities in China. Migrant workers face tremendous legal barriers under the hukou system, which excludes non-urban residents from social welfare. Grassroots migrant NGOs emerged as an informal way to provide legal protection and services to migrant workers, however, they have constantly coped with government scrutiny. On January 1st, 2017, the New NGO Law was implemented with the intention of legalizing grassroots NGOs’ status and activities.
+    	 </p>
+    </main>
+
+
+    	<footer class="myname">
+    			 CSC 174 Advanced Front-End Web Development 
+ 		</footer>
+
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+ <script src="js/menu-highlighter.js"></script>
+</div>
+</body>
+</html>
