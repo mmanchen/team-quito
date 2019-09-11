d979898530ff74ac2d5c3ac7d294442a9a46ff4a
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 16:19:00 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Shelley, Dewey, Maria | Lab 2 </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Maria Mancheno </h1>
+			
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+            <a href= "shelley-index.php">
+            <li>Shelley</li>
+            <a href= "dewey_index.php">
+            <li>Dewey</li>
+            <a href= "maria-index.php">
+			<li>Maria</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>We're students at the University of Rochester. </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
