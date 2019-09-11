72190ce0d9c5a54eba4bbf25c2a77b0053e16007
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 15:53:22 -0400

    Create maria-high-school.php

---
+++ maria-high-school.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Maria Mancheno | Lab 1: Initial Personal Website </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.html">
+			<h1> Maria Mancheno </h1>
+			<img src= "photo-maria.jpg" alt="maria">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>University of Rochester</li>
+			<li>Senior</li>
+			<li>Digital Media Studies</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> High School </h1>
+
+		<p>In Ecuador, kinder, primary and secondary are only one school. So, I can say I went to the same school for 14 years. The name of my school was American School of Quito, which is the city where I’m from. My favorite teacher was my Physics teacher from my two last years of high school. I also did the International Baccalaureate Program, where you have to choose two natural science classes and a social science class, or vice versa. I ended up choosing Economics, Physics and History. It was a good combination but very demanding as well. Overall my experience at the school was great. I met a lot of people, who’ve been my friends since we were 4 years old. I know the school made a good job in getting me prepared for college life. 
+</p>
+
+	</main>
+
+		
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
