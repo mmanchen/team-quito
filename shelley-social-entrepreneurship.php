8ea9de5b2fbd775fcddb1f64f6a73def27890368
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 20:21:17 -0400

    Update shelley-social-entrepreneurship.php

--- shelley-social-entrepreneurship.php
+++ shelley-social-entrepreneurship.php
@@ -1,48 +1,44 @@
-<?php include "inc/html-top.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Xueying Chang  </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
-
-<body>
-	<div class="container"> 
 
 	<header>
-		<a href="index.php">	
-	    <h1> Xueying Chen </h1>
-	    <img src="images/shelley.jpg" alt="shelley-portrait">
-	    </a>
-    </header>
+		<a href= "index.html">
+			<h1> Xueying Chang </h1>
+			<img src= "images/shelley.jpg" alt="shelley">
+		</a>
+	</header>
 
-    <aside>
-    	<ul>
-		<li>
-			Social Entrepreneur
-		</li>
-		<li>
-			Yoga and Meditation Enthusiast
-		</li>
-		<li>
-			Island Traveler
-		</li>
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Social Entrepreneur</li>
+			<li>Yoga and Meditation Enthusiast</li>
+			<li>Becoming an Island Hopper</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Social Entrepreneurship </h1>
+
+		<p>I am a researcher, activist and writer of social entrepreneurship and social innovation. I worked on a research project in Beijing and Shenzhen investigating how Chinese social entrepreneurs help migrant workers integrate into urban society in China. The phenomenon of large-scale rural-urban migration has brought 250 million migrant workers into urban cities in China. Migrant workers face tremendous legal barriers under the hukou system, which excludes non-urban residents from social welfare. Grassroots migrant NGOs emerged as an informal way to provide legal protection and services to migrant workers, however, they have constantly coped with government scrutiny. On January 1st, 2017, the New NGO Law was implemented with the intention of legalizing grassroots NGOs’ status and activities.
+</p>
+
+	</main>
+
 		
-	</ul>	
-    </aside>
-   	
 
- <?php include "inc/nav.php"; ?>
- 
-    <main>
-    	<h1> Social Entrepreneurship </h1>
-    	 <p>I am a researcher, activist and writer of social entrepreneurship and social innovation. I worked on a research project in Beijing and Shenzhen investigating how Chinese social entrepreneurs help migrant workers integrate into urban society in China. The phenomenon of large-scale rural-urban migration has brought 250 million migrant workers into urban cities in China. Migrant workers face tremendous legal barriers under the hukou system, which excludes non-urban residents from social welfare. Grassroots migrant NGOs emerged as an informal way to provide legal protection and services to migrant workers, however, they have constantly coped with government scrutiny. On January 1st, 2017, the New NGO Law was implemented with the intention of legalizing grassroots NGOs’ status and activities.
-    	 </p>
-    </main>
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
 
-
-    	<footer class="myname">
-    			 CSC 174 Advanced Front-End Web Development 
- 		</footer>
-
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
- <script src="js/menu-highlighter.js"></script>
-</div>
-</body>
-</html>
+</body></html>
