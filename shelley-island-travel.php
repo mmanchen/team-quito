31eb97ba20d6f0890c22b63b29cb3bf38348acaa
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 20:21:13 -0400

    Update shelley-island-travel.php

--- shelley-island-travel.php
+++ shelley-island-travel.php
@@ -1,47 +1,44 @@
-<?php include "inc/html-top.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Xueying Chang </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
-<body>
-	<div class="container"> 
 
 	<header>
-		<a href="index.php">	
-	    <h1> Xueying Chen </h1>
-	    <img src="images/shelley.jpg" alt="shelley-portrait"> 
-	    </a>
-    </header>
-
-    <aside>
-    	<ul>
-		<li>
-			Social Entrepreneur
-		</li>
-		<li>
-			Yoga and Meditation Enthusiast
-		</li>
-		<li>
-			Island Traveler
-		</li>
-		
-	</ul>	
-    </aside>
-   	
+		<a href= "index.html">
+			<h1> Xueying Chang </h1>
+			<img src= "images/shelley.jpg" alt="shelley">
+		</a>
+	</header>
 
 <?php include "inc/nav.php"; ?>
- 
-    <main>
-    	<h2> Becoming an island hopper </h2>
-    	<p>The Turks and Caicos Islands are serene, picturesque, charming, and the perfect vacation destination for adventurers and those who like to take a break from the bustling city life. I recently went there for a spring break trip, and I immediately fell in love with the turquoise ocean and soft white sandy beaches. We will break down our trip for you so that you can get the most out of your vacation in beautiful Turks & Caicos!
-		</p>
-    </main>
 
-    	<footer class="myname">
-    			 CSC 174 Advanced Front-End Web Development 
- 		</footer>
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
- <script src="js/menu-highlighter.js"></script>
+	<aside>
+		<ul>
+			<li>Social Entrepreneur</li>
+			<li>Yoga and Meditation Enthusiast</li>
+			<li>Becoming an Island Hopper</li>
+		</ul>
+	</aside>
 
 
-</div>
-</body>
-</html>
+	<main>
+		<h1> Becoming an Island Hopper </h1>
+
+		<p>The Turks and Caicos Islands are serene, picturesque, charming, and the perfect vacation destination for adventurers and those who like to take a break from the bustling city life. I recently went there for a spring break trip, and I immediately fell in love with the turquoise ocean and soft white sandy beaches. We will break down our trip for you so that you can get the most out of your vacation in beautiful Turks & Caicos!
+</p>
+
+	</main>
+
+		
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
