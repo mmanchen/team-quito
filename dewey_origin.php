a3290d9be9d8ecf45eddb3b12bb57bcc26ddbd72
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 20:49:02 -0400

    Update dewey_origin.php

--- dewey_origin.php
+++ dewey_origin.php
@@ -1,69 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Dewey Bazirake </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
 
-<?php include "inc/html-top.inc"; ?>
+	<header>
+		<a href= "index.html">
+			<h1> Dewey Bazirake </h1>
+			<img src= "images/dewey_pic.jpg" alt="dewey">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Dewey Bazirake</li>
+			<li>Data Scientist & Systems Analyst</li>
+			<li>Business Analytics + Comp Sci</li>
+		</ul>
+	</aside>
 
 
+	<main>
+		<h1> Origin </h1>
 
-
-
-<body class="container">
-
-
-
-<header>
-	<a href="index.php/">
-		<h1>Dewey Bazirake</h1>
-		<img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake">
-	</a>
-</header>
-
-<?php include "inc/nav.inc"; ?>
-
-<aside>
-
-<ul>
-
-	<li>Dewey Bazirake</li>
-
-	<li>Data Scientist & Systems Analyst</li>
-
-	<li>Business Analytics + Comp Sci</li>
-
-</ul>
-
-</aside>
-
-
-
-
-<main>
-
-<h1>Innovative Works</h1>
-
-
-<p>
-Uganda is located in the heart of Africa. It is home to 42 million people and the population is growing at an average of 3.7% per year (as of 2018). That means you should all expect more Ugandan babies in the upcoming years. Uganda is blessed with abundant sun, freshwater & a rich cultural diversity of people. We are home to 56 traditional tribes which all have respective languages, dances, cultural clothing & values. Despite the diversity, the people all thrive in unison. Uganda is known for its rich wildlife (including lions, giraffes, buffalos, crocodiles, hippopotamuses, etc), rich bird diversity and fresh-water lakes including Lake Victoria, the largest lake in Africa - mother to River Nile that is shared with Tanzania & Kenya. Uganda is a peaceful country that is vibrant with opportunity for growth habiting a people that treasure and preserve nature.
+		<p>Uganda is located in the heart of Africa. It is home to 42 million people and the population is growing at an average of 3.7% per year (as of 2018). That means you should all expect more Ugandan babies in the upcoming years. Uganda is blessed with abundant sun, freshwater & a rich cultural diversity of people. We are home to 56 traditional tribes which all have respective languages, dances, cultural clothing & values. Despite the diversity, the people all thrive in unison. Uganda is known for its rich wildlife (including lions, giraffes, buffalos, crocodiles, hippopotamuses, etc), rich bird diversity and fresh-water lakes including Lake Victoria, the largest lake in Africa - mother to River Nile that is shared with Tanzania & Kenya. Uganda is a peaceful country that is vibrant with opportunity for growth habiting a people that treasure and preserve nature.
 </p>
 
-</main>
+	</main>
 
+		
 
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
 
+</body></html>
 
-
-<?php include "inc/nav.inc"; ?>
-<?php include "inc/scripts.php"; ?>
-
-
-<footer>
-
-	<p><strong>CSC 174: Advanced Front-end Web</strong></footer>
-
-
-</footer>
-
-
-</body>
-
-
-</html>
