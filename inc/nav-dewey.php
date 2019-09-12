3cb560313cddd1f91c859fce4aed0490d81a6870
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 20:49:06 -0400

    Update nav-dewey.php

--- inc/nav-dewey.php
+++ inc/nav-dewey.php
@@ -10,7 +10,8 @@
 			<a href="dewey_origin.php">Origin</a>
 		</li>
 		<li>
-			<a href="dewey_technical_background"> Technical Background </a>
+			<a href="dewey_technical_background.php"> Technical Background </a>
 		</li>
 	</ul>	
 	</nav>
+
