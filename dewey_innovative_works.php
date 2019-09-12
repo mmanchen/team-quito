d2fa5bd9a86092b24b08047abb8fc37799c508a4
mmanchen <mmanchen@u.rochester.edu>
2019-09-11 20:49:00 -0400

    Update dewey_innovative_works.php

--- dewey_innovative_works.php
+++ dewey_innovative_works.php
@@ -1,70 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Dewey Bazirake </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
 
-<?php include "inc/html-top.inc"; ?>
+	<header>
+		<a href= "index.html">
+			<h1> Dewey Bazirake </h1>
+			<img src= "images/dewey_pic.jpg" alt="dewey">
+		</a>
+	</header>
+
+<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Dewey Bazirake</li>
+			<li>Data Scientist & Systems Analyst</li>
+			<li>Business Analytics + Comp Sci</li>
+		</ul>
+	</aside>
 
 
+	<main>
+		<h1> Innovative Works </h1>
 
-
-
-<body class="container">
-
-
-
-<header>
-	<a href="index.php/">
-		<h1>Dewey Bazirake</h1>
-		<img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake">
-	</a>
-</header>
-
-<?php include "inc/nav.inc"; ?>
-
-<aside>
-
-<ul>
-
-	<li>Dewey Bazirake</li>
-
-	<li>Data Scientist & Systems Analyst</li>
-
-	<li>Business Analytics + Comp Sci</li>
-
-</ul>
-
-</aside>
-
-
-
-
-<main>
-
-<h1>Innovative Works</h1>
-
-
-<p>
-
-I take pleasure in starting and managing companies. So far, I have two companies legally registered and undergoing development. One is a non-profit entity aimed at empowering young people in my home country and another is a for-profit e-commerce entity in the construction & infrastructure industry. A lot more will be started as long as I am alive. I have attempted many startups in the travel & environmental sustainability industries. As time progresses, I hope to startup & co-found companies in the Robotics, Real Estate, Energy, Agriculture & Logistics industries. The hope is that these companies will get to improve the lives of our humanity and delegate the profit to the right reasons that advance a civilization in the appropriate direction.
+		<p>I take pleasure in starting and managing companies. So far, I have two companies legally registered and undergoing development. One is a non-profit entity aimed at empowering young people in my home country and another is a for-profit e-commerce entity in the construction & infrastructure industry. A lot more will be started as long as I am alive. I have attempted many startups in the travel & environmental sustainability industries. As time progresses, I hope to startup & co-found companies in the Robotics, Real Estate, Energy, Agriculture & Logistics industries. The hope is that these companies will get to improve the lives of our humanity and delegate the profit to the right reasons that advance a civilization in the appropriate direction.
 </p>
 
-</main>
+	</main>
 
+		
 
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
 
-
-
-<?php include "inc/nav.inc"; ?>
-<?php include "inc/scripts.php"; ?>
-
-
-<footer>
-
-	<p><strong>CSC 174: Advanced Front-end Web</strong></footer>
-
-
-</footer>
-
-
-</body>
-
-
-</html>
+</body></html>
