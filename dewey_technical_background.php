50151cc4e4d953b4bd9ccf0492de2eccd8e04f6e
mmanchen <mmanchen@u.rochester.edu>
2019-09-13 14:32:11 -0400

    Update dewey_technical_background.php

--- dewey_technical_background.php
+++ dewey_technical_background.php
@@ -1,41 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "inc/nav-shelley.php"; ?>
+	<meta charset="utf-8">
+	<title> Dewey Bazirake </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
-<?php include "inc/html-top.php"; ?>
-
-<body class="container">
 
 	<header>
-	    <a href="dewey_index.php">
-	    <h1>  Dewey Bazirake </h1>
-	    <a href="dewey_index.php"><img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake"></a>
+		<a href= "index.html">
+			<h1> Dewey Bazirake </h1>
+			<img src= "images/dewey_pic.jpg" alt="dewey">
+		</a>
+	</header>
 
-	    </a>
-    </header>
+<?php include "inc/nav.php"; ?>
 
-    <?php include "inc/nav-dewey.php"; ?>
-
-<aside>
-<ul>
+	<aside>
+		<ul>
 		<li>Data Scientist & Systems Analyst</li>
 		<li>Python, Web Design & Machine Learning</li>
 		<li>Business Analytics + Comp Sci</li>
-	</ul>
-  </aside>
+		</ul>
+	</aside>
 
 
-<main>
+	<main>
+		<h1> Technical Background  </h1>
 
-<h1>Technical Background</h1>
-<p>
-My love for numbers and making a change in the world has resulted in me taking an interest in Business, Computer Science & Robotics. During my Junior year, I went to study abroad in Shanghai, China which was a life changing experience. Aside from the great food that I enjoyed, I got a chance to put my python programming skills into practice. I worked with a startup called, MIRO AI - a sports AI company. They focused on image processing of athletes in marathons. It is through this experience I got a chance to learn more about Deep Learning, Artificial Intelligence & the power of Big Data. From then on, my life has never been the same again. I am always looking for opportunities to enhance my knowledge and skills in the Data & the Technology sector. The language I am getting fluent at: python, statistics, machine learning and databases. My hope is that I can work work with numerous companies who have access to large data so that I can do what I do best ~ organize it, analyze it & interpret it to derive the best value.
+		<p>My love for numbers and making a change in the world has resulted in me taking an interest in Business, Computer Science & Robotics. During my Junior year, I went to study abroad in Shanghai, China which was a life changing experience. Aside from the great food that I enjoyed, I got a chance to put my python programming skills into practice. I worked with a startup called, MIRO AI - a sports AI company. They focused on image processing of athletes in marathons. It is through this experience I got a chance to learn more about Deep Learning, Artificial Intelligence & the power of Big Data. From then on, my life has never been the same again. I am always looking for opportunities to enhance my knowledge and skills in the Data & the Technology sector. The language I am getting fluent at: python, statistics, machine learning and databases. My hope is that I can work work with numerous companies who have access to large data so that I can do what I do best ~ organize it, analyze it & interpret it to derive the best value.
 </p>
-</main>
 
-<?php include "inc/scripts.php"; ?>
+	</main>
 
-<footer>
-  <strong>CSC 174: Advanced Front-end Web Design and Development</strong>
-</footer>
+		
 
-</body>
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
 
-</html>
+</body></html>
