05644296d3c021690385732674e6dde333e48f05
Baz D <deweybazirake@gmail.com>
2019-09-13 10:41:04 -0400

    Update html-top.php

--- inc/html-top.php
+++ inc/html-top.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8">
 		
 		<title>Team Quito | Personal Websites </title>
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/styles.css">
 		<script src="js/menu-highlighter.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
