ea840f002a211d5dbc187ce2f40ed7cb93a16868
Baz D <deweybazirake@gmail.com>
2019-09-13 10:23:04 -0400

    Update html-top.php

--- inc/html-top.php
+++ inc/html-top.php
@@ -5,7 +5,7 @@
 	<head>
 		<meta charset="utf-8">
 		
-		<title>Xueying Chen| Lab 1:Initial Personal Website </title>
+		<title>Team Quito | Personal Websites </title>
 		<link rel="stylesheet" href="css/style.css">
 		<script src="js/menu-highlighter.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
