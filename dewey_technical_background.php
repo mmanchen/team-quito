1179ad7d4cfffcabe3bddf629d997b65a41bc67b
Baz D <deweybazirake@gmail.com>
2019-09-13 11:40:39 -0400

    Update dewey_technical_background.php

--- dewey_technical_background.php
+++ dewey_technical_background.php
@@ -24,7 +24,7 @@
 
 <main>
 
-<h1>Technical Background<h1/>
+<h1>Technical Background</h1>
 <p>
 My love for numbers and making a change in the world has resulted in me taking an interest in Business, Computer Science & Robotics. During my Junior year, I went to study abroad in Shanghai, China which was a life changing experience. Aside from the great food that I enjoyed, I got a chance to put my python programming skills into practice. I worked with a startup called, MIRO AI - a sports AI company. They focused on image processing of athletes in marathons. It is through this experience I got a chance to learn more about Deep Learning, Artificial Intelligence & the power of Big Data. From then on, my life has never been the same again. I am always looking for opportunities to enhance my knowledge and skills in the Data & the Technology sector. The language I am getting fluent at: python, statistics, machine learning and databases. My hope is that I can work work with numerous companies who have access to large data so that I can do what I do best ~ organize it, analyze it & interpret it to derive the best value.
 </p>
