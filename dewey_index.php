6d9a7b3d5ad23e9a798689e1d430fa7cea5e2eac
Baz D <deweybazirake@gmail.com>
2019-09-13 10:43:20 -0400

    Update dewey_index.php

--- dewey_index.php
+++ dewey_index.php
@@ -1,42 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<?php include "inc/nav-dewey.php"; ?>
-	<meta charset="utf-8">
-	<title> Dewey Bazirake </title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
-	<script src="menu-highlighter.js"></script>
-</head>
-<body class= "container">
 
+<?php include "inc/html-top.php"; ?>
+
+<body class="container">
 
 	<header>
-		<a href= "index.php">
-			<h1> Dewey Bazirake </h1>
-			<img src= "images/dewey_pic.jpg" alt="dewey">
-			
-		</a>
-	</header>
+	    <a href="dewey_index.php">
+	    <h1>  Dewey Bazirake </h1>
+	    <a href="dewey_index.php"><img src="images/dewey_pic.jpg" alt="Picture of Dewey Bazirake"></a>
 
-<?php include "inc/nav.php"; ?>
+	    </a>
+    </header>
 
-	<aside>
-		<ul>
-			<li>Data Scientist & Systems Analyst</li>
-			<li>Python, Web Design & Machine Learning</li>
-			<li>Business Analytics + Comp Sci </li>
-		</ul>
-	</aside>
+    <?php include "inc/nav-dewey.php"; ?>
 
-
-	<main>
-		<p>I am an international student from Uganda, East Africa currently studying in the United States. My belief in education to make a positive change has always been a driving factor in my academics, work and travel. My hope is that to learn the necessary skills in computer science, finance and management to lead organizations and companies that will make a fundamental change in our world. We are becoming a more interconnected world thanks to technology, common language and easier transportation mechanisms. This gives me optimism as we can always work holistically as humans to advance in technology, protect our environment for future generations and lead a new era of sustainable prosperity. For more details into my background, technical skills, origin and my entrepreneurship journey, check out the other pages. 
- </p>
-	</main>
+<aside>
+<ul>
+		<li>Data Scientist & Systems Analyst</li>
+		<li>Python, Web Design & Machine Learning</li>
+		<li>Business Analytics + Comp Sci</li>
+	</ul>
+  </aside>
 
 
-	<?php include "inc/scripts.php";?>
-	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
-	
+<main>
+<p>
 
-</body></html>
+I am an international student from Uganda, East Africa currently studying in the United States. My belief in education to make a positive change has always been a driving factor in my academics, work and travel. My hope is that to learn the necessary skills in computer science, finance and management to lead organizations and companies that will make a fundamental change in our world. We are becoming a more interconnected world thanks to technology, common language and easier transportation mechanisms. This gives me optimism as we can always work holistically as humans to advance in technology, protect our environment for future generations and lead a new era of sustainable prosperity. For more details into my background, technical skills, origin and my entrepreneurship journey, check out the other pages.
+</p>
+</main>
+
+<?php include "inc/scripts.php"; ?>
+
+<footer>
+  <strong>CSC 174: Advanced Front-end Web Design and Development</strong>
+</footer>
+
+</body>
+
+</html>
